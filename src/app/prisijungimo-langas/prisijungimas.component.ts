875d4b5e0f4c10df018811f13918eb843eee8d37
JustasB <102652761+JustasBalandis@users.noreply.github.com>
2024-05-24 09:26:05 -0700

    Add files via upload

---
+++ src/app/prisijungimo-langas/prisijungimas.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './prisijungimas.component.html',
+  styleUrl: './prisijungimas.component.css'
+})
+export class PrisijungimasComponent {
+  naujaKelione = false;
+  pasirinktasPradziosTaskas: string = '';
+  pasirinktasPabaigosTaskas: string = '';
+  pasirinktaData: Date;
+
+  atidarytiKelionesPlanavimoLangas() {
+    this.naujaKelione = true;
+  }
+
+  pasirinktiNuomotiAutomobili() {
+    // Čia galite įgyvendinti automobilių nuomos pasiūlymų langą
+    console.log('Pasirinkti nuomoti automobilį');
+  }
+
+  pasirinktiRegistruotiBagaza() {
+    // Čia galite įgyvendinti bagažo registravimo langą
+    console.log('Pasirinkti registruoti bagažą');
+  }
+
+  pasirinktiNuomuotiViesbuti() {
+    // Čia galite įgyvendinti viešbučių peržiūros langą
+    console.log('Pasirinkti nuomuoti viešbutį');
+  }
+
+  pasirinktiPridetiPapildomasPaslaugas() {
+    // Čia galite įgyvendinti papildomų paslaugų peržiūros langą
+    console.log('Pasirinkti pridėti papildomas paslaugas');
+  }
+
+  issaugotiKelionesPlana() {
+    // Čia galite įgyvendinti kelionės plano išsaugojimą
+    console.log('Išsaugoti kelionės planą');
+  }
+}
